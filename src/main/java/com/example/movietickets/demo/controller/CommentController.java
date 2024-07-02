87bc86eb56a0629406f52ee0f17dae26f414e6b1
Bui Chi Bao <buichibao1601@gmail.com>
2024-07-03 02:28:46 +0700

    merge

--- src/main/java/com/example/movietickets/demo/controller/CommentController.java
+++ src/main/java/com/example/movietickets/demo/controller/CommentController.java
@@ -42,44 +42,44 @@ public class CommentController {
     @Autowired
     private  final CategoryService categoryService;
 
-//    @PostMapping("/blog-details/{id}/comment")
-//    public String addComment(@PathVariable Long id, @Valid @ModelAttribute Comment comment,
-//                             BindingResult result, Model model, RedirectAttributes redirectAttributes) {
-//
-//        // Kiểm tra lỗi binding
-//        /*if (result.hasErrors()) {
-//            Blog blog = blogService.findBlogWithId(id);
-//            model.addAttribute("blog", blog);
-//            model.addAttribute("categories", commentService.getAllCommentsByPostId(id));
-//            return "blog/blog-detail";
-//        }*/
-//
-//        // Lấy thông tin blog từ id
-//        Blog blog = blogService.findBlogWithId(id);
-//
-//        // Kiểm tra và lấy thông tin người dùng đăng nhập hiện tại
-//        User user = userService.getCurrentUser();
-//
-//        List<Category> categories = categoryService.getAllCategories();
-//
-//        model.addAttribute("categories", categories);
-//
-//        // Thiết lập thông tin cho comment
-//        Comment newComment = new Comment();
-//        newComment.setBlog(blog);
-//        newComment.setUser(user);
-//        newComment.setContent(comment.getContent());
-//        newComment.setDate(LocalDateTime.now());
-//
-//        // Lưu comment vào cơ sở dữ liệu
-//        commentService.saveComment(newComment);
-//
-//        // Điều hướng lại đến trang chi tiết blog
-//        return "redirect:/blog/blog-details/" + id;
-//    }
-
-
     @PostMapping("/blog-details/{id}/comment")
+    public String addComment(@PathVariable Long id, @Valid @ModelAttribute Comment comment,
+                             BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+
+        // Kiểm tra lỗi binding
+        /*if (result.hasErrors()) {
+            Blog blog = blogService.findBlogWithId(id);
+            model.addAttribute("blog", blog);
+            model.addAttribute("categories", commentService.getAllCommentsByPostId(id));
+            return "blog/blog-detail";
+        }*/
+
+        // Lấy thông tin blog từ id
+        Blog blog = blogService.findBlogWithId(id);
+
+        // Kiểm tra và lấy thông tin người dùng đăng nhập hiện tại
+        User user = userService.getCurrentUser();
+
+        List<Category> categories = categoryService.getAllCategories();
+
+        model.addAttribute("categories", categories);
+
+        // Thiết lập thông tin cho comment
+        Comment newComment = new Comment();
+        newComment.setBlog(blog);
+        newComment.setUser(user);
+        newComment.setContent(comment.getContent());
+        newComment.setDate(LocalDateTime.now());
+
+        // Lưu comment vào cơ sở dữ liệu
+        commentService.saveComment(newComment);
+
+        // Điều hướng lại đến trang chi tiết blog
+        return "redirect:/blog/blog-details/" + id;
+    }
+
+
+    /*@PostMapping("/blog-details/{id}/comment")
     @ResponseBody
     public ResponseEntity<Map<String, Object>> addComment(@PathVariable Long id, @Valid @ModelAttribute Comment comment,
                                                           BindingResult result) {
@@ -131,7 +131,7 @@ public class CommentController {
         response.put("comment", commentData);
 
         return ResponseEntity.ok(response);
-    }
+    }*/
 
     @PostMapping("/blog-details/{id}/delete/{commentId}")
     public String deleteComment(@PathVariable("commentId") Long commentId, @PathVariable("id") Long blogId, RedirectAttributes redirectAttributes) {
